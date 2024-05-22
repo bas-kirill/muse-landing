d9610ebcd6db08148266b6e8fc43e147c0537670
skywalker <baskirill.an@gmail.com>
2024-05-22 16:13:59 +0300

    feat(our products): add non-responsive UI for "Our Products"

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./components/header/Header";
 import HeroSection from "./components/hero/HeroSection";
+import OurProducts from "./components/products/OurProducts";
 
 function App() {
     return (
         <div id="muse-group-landing">
             <Header/>
             <HeroSection/>
+            <OurProducts />
         </div>
     );
 }
---
+++ src/components/products/BlueRightCarret.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BlueRightCarret = () => {
+    return (
+        <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clipPath="url(#clip0_4224_26)">
+                <path d="M5.25 3.125L9.625 7.5L5.25 11.875" stroke="#3366FF" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+            </g>
+            <defs>
+                <clipPath id="clip0_4224_26">
+                    <rect width="14" height="14" fill="white" transform="translate(0 0.5)"/>
+                </clipPath>
+            </defs>
+        </svg>
+    );
+}
+
+export default BlueRightCarret;
